c170991f515af916a9047604a6b087e6b8ef40ab
Mark Kluber <145130822+mkluber@users.noreply.github.com>
2023-12-25 23:12:25 +0100

    .

--- main.tf
+++ main.tf
@@ -6,107 +6,117 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "fortunevpc"
-  cidr = "137.137.0.0/16"
+  cidr = var.cidr
 
-  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-  public_subnets = ["137.137.1.0/24", "137.137.2.0/24", "137.137.3.0/24"]
+  azs             = var.azs
+  public_subnets = var.public_subnets
   create_igw = "true"
   default_route_table_name = "fortuneroute"
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
+module "security-group_http-80" {
+  source  = "terraform-aws-modules/security-group/aws//modules/http-80"
 
-  # Autoscaling group
-  name = "fortunegroup"
+  name        = "http-sg"
+  description = "Security group with HTTP ports open for everybody (IPv4 CIDR), egress ports are all world open"
+  vpc_id      = module.vpc.vpc_id
 
-  min_size                  = 1
-  max_size                  = 3
-  desired_capacity          = 2
-  wait_for_capacity_timeout = 0
-  health_check_type         = "EC2"
-  vpc_zone_identifier       = module.vpc.public_subnets ### CONTINUE FROM HERE ###
-
-  initial_lifecycle_hooks = [
-    {
-      name                  = "ExampleStartupLifeCycleHook"
-      default_result        = "CONTINUE"
-      heartbeat_timeout     = 60
-      lifecycle_transition  = "autoscaling:EC2_INSTANCE_LAUNCHING"
-      notification_metadata = jsonencode({ "hello" = "world" })
-    },
-    {
-      name                  = "ExampleTerminationLifeCycleHook"
-      default_result        = "CONTINUE"
-      heartbeat_timeout     = 180
-      lifecycle_transition  = "autoscaling:EC2_INSTANCE_TERMINATING"
-      notification_metadata = jsonencode({ "goodbye" = "world" })
-    }
-  ]
-
-  instance_refresh = {
-    strategy = "Rolling"
-    preferences = {
-      checkpoint_delay       = 600
-      checkpoint_percentages = [35, 70, 100]
-      instance_warmup        = 300
-      min_healthy_percentage = 50
-    }
-    triggers = ["tag"]
-  }
-
-  # Launch template
-  launch_template_name        = "fortunetemplate"
-  launch_template_description = "Launch template example"
-  update_default_version      = true
-  target_group_arns         = [module.alb.target_groups["fortunetarget"].arn]
-
-  image_id          = "ami-00eb698aea3e796bc"
-  instance_type     = "t2.micro"
-  security_groups   = ["sg-084412ea7d4375dde"]
-
-  # IAM role & instance profile
-  create_iam_instance_profile = false
-  iam_instance_profile_arn = "arn:aws:iam::385071190480:instance-profile/FortuneWebServerDynamodbRole"
-
-  # This will ensure imdsv2 is enabled, required, and a single hop which is aws security
-  # best practices
-  # See https://docs.aws.amazon.com/securityhub/latest/userguide/autoscaling-controls.html#autoscaling-4
-  metadata_options = {
-    http_endpoint               = "enabled"
-    http_tokens                 = "required"
-    http_put_response_hop_limit = 1
-  }
+  ingress_cidr_blocks = var.ingress_cidr_blocks
 }
 
+# module "autoscaling" {
+#   source  = "terraform-aws-modules/autoscaling/aws"
 
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+#   # Autoscaling group
+#   name = "fortunegroup"
 
-  name    = "fortunelb"
-  vpc_id  = "vpc-0a2da1530815bf09c"
-  create_security_group = "false"
-  security_groups = ["sg-084412ea7d4375dde"]
-  subnets = ["subnet-06cdce1d8e6b71368", "subnet-02449290dd55a46b1", "subnet-06d17cf8cdfc78514"]
-  enable_deletion_protection = "false"
+#   min_size                  = 1
+#   max_size                  = 3
+#   desired_capacity          = 2
+#   wait_for_capacity_timeout = 0
+#   health_check_type         = "EC2"
+#   vpc_zone_identifier       = module.vpc.public_subnets
 
-  listeners = {
-    fortunelistener = {
-    port     = 80
-    protocol = "HTTP"
-      forward = {
-        target_group_key = "fortunetarget"
-      }
-    }
-  }
+#   initial_lifecycle_hooks = [
+#     {
+#       name                  = "ExampleStartupLifeCycleHook"
+#       default_result        = "CONTINUE"
+#       heartbeat_timeout     = 60
+#       lifecycle_transition  = "autoscaling:EC2_INSTANCE_LAUNCHING"
+#       notification_metadata = jsonencode({ "hello" = "world" })
+#     },
+#     {
+#       name                  = "ExampleTerminationLifeCycleHook"
+#       default_result        = "CONTINUE"
+#       heartbeat_timeout     = 180
+#       lifecycle_transition  = "autoscaling:EC2_INSTANCE_TERMINATING"
+#       notification_metadata = jsonencode({ "goodbye" = "world" })
+#     }
+#   ]
 
-  target_groups = {
-    fortunetarget = {
-      name             = "fortunetarget"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      create_attachment = "false"
-    }
-  }
-}
+#   instance_refresh = {
+#     strategy = "Rolling"
+#     preferences = {
+#       checkpoint_delay       = 600
+#       checkpoint_percentages = [35, 70, 100]
+#       instance_warmup        = 300
+#       min_healthy_percentage = 50
+#     }
+#     triggers = ["tag"]
+#   }
+
+#   # Launch template
+#   launch_template_name        = "fortunetemplate"
+#   launch_template_description = "Launch template example"
+#   update_default_version      = true
+#   target_group_arns         = module.alb.target_groups["fortunetarget"].arn
+
+#   image_id          = var.image_id
+#   instance_type     = var.instance_type
+#   security_groups   = module.security-group_http-80.security_group_id
+
+#   # IAM role & instance profile
+#   create_iam_instance_profile = true
+#   iam_instance_profile_arn = "arn:aws:iam::385071190480:instance-profile/FortuneWebServerDynamodbRole"
+
+#   # This will ensure imdsv2 is enabled, required, and a single hop which is aws security
+#   # best practices
+#   # See https://docs.aws.amazon.com/securityhub/latest/userguide/autoscaling-controls.html#autoscaling-4
+#   metadata_options = {
+#     http_endpoint               = "enabled"
+#     http_tokens                 = "required"
+#     http_put_response_hop_limit = 1
+#   }
+# }
+
+
+# module "alb" {
+#   source = "terraform-aws-modules/alb/aws"
+
+#   name    = "fortunelb"
+#   vpc_id  = module.vpc.vpc_id
+#   create_security_group = "false"
+#   security_groups = ["sg-084412ea7d4375dde"]
+#   subnets = module.vpc.public_subnets
+#   enable_deletion_protection = "false"
+
+#   listeners = {
+#     fortunelistener = {
+#     port     = 80
+#     protocol = "HTTP"
+#       forward = {
+#         target_group_key = "fortunetarget"
+#       }
+#     }
+#   }
+
+#   target_groups = {
+#     fortunetarget = {
+#       name             = "fortunetarget"
+#       protocol         = "HTTP"
+#       port             = 80
+#       target_type      = "instance"
+#       create_attachment = "false"
+#     }
+#   }
+# }
--- variables.tf
+++ variables.tf
@@ -3,41 +3,26 @@ variable "region" {
   default     = "eu-central-1"
 }
 
-# variable "instance_type" {
-#   description = "Type of EC2 instance to provision"
-#   default     = "t2.micro"
-# }
+variable "cidr" {
+  description = "CIDR value assigned to the VPC"
+}
 
-# variable "instance_name" {
-#   description = "EC2 instance name"
-#   default     = "Provisioned by Terraform"
-# }
+variable "azs" {
+  description = "Availability zones assigned to the VPC"
+}
 
-# variable "lb_security_groups" {
-#   description = "List of security groups assigned to the load balancer"
-# }
+variable "public_subnets" {
+  description = "Public subnets created by the VPC"
+}
 
-# variable "lb_subnets" {
-#   description = "List of subnets assigned to the load balancer"
-# }
+variable "ingress_cidr_blocks" {
+  description = "The cidr block used for inbound rules in the newly created security group"
+}
 
+variable "image_id" {
+  description = "AMI used by the EC2 instances"
+}
 
-# variable "lb_target_group_vpc_id" {
-#   description = "The VPC id used by the load balancer target group"
-# }
-
-# variable "instance_profile_name" {
-#   description = "The instance profile used by the EC2 instances"
-# }
-
-# variable "ami_id" {
-#   description = "The AMI used by the EC2 instances"
-# }
-
-# variable "launch_template_vpc_sg_ids" {
-#   description = "List of VPC security group ids used by the launch template"
-# }
-
-# variable "autoscaling_group_availability_zones" {
-#   description = "List of availability zones used by the autoscaling group"
-# }
+variable "instance_type" {
+  description = "The type of instance for EC2 instances"
+}
