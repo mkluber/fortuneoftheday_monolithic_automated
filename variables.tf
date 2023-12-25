d5cf2cdab9c9ad7506ddb31e5a385065ec6bf267
Mark Kluber <145130822+mkluber@users.noreply.github.com>
2023-12-25 12:03:22 +0100

    .

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-# variable "region" {
-#   description = "AWS region"
-#   default     = "eu-central-1"
-# }
+variable "region" {
+  description = "AWS region"
+  default     = "eu-central-1"
+}
 
 # variable "instance_type" {
 #   description = "Type of EC2 instance to provision"
